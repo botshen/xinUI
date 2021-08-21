db931263727f9f13637848d98f80c86cd8d76cd4
Xin shen <363088847@qq.com>
2021-08-21 16:42:16 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -14,7 +15,7 @@ export default {
     iconPosition: {
       type: String,
       default: 'left',
-      validator (value) {
+      validator(value) {
         return value === 'left' || value === 'right'
       }
     }
@@ -22,19 +23,62 @@ export default {
 }
 </script>
 <style lang="scss">
-.g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
-  border-radius: var(--border-radius); border: 1px solid var(--border-color);
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
   background: var(--button-bg);
-  display: inline-flex; justify-content: center; align-items: center;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   vertical-align: middle;
-  &:hover { border-color: var(--border-color-hover); }
-  &:active { background-color: var(--button-active-bg); }
-  &:focus { outline: none; }
-  > .content { order: 2; }
-  > .icon { order: 1; margin-right: .1em; }
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+
   &.icon-right {
-    > .content { order: 1; }
-    > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+    > .content {
+      order: 1;
+    }
+
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
+  }
+
+  .loading {
+    animation: spin 2s infinite linear;
   }
 }
 </style>
