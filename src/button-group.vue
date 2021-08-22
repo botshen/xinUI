c5f4ed6cae586ac2e24e595ffcf488266d7a8f2d
Xin shen <363088847@qq.com>
2021-08-22 17:58:53 +0800

    添加 input.vue 及其样式

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,11 +1,12 @@
 <template>
-  <div className="g-button-group">
+  <div class="g-button-group">
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  mounted() {
+  name: 'GuluButtonGroup',
+  mounted () {
     for (let node of this.$el.children) {
       let name = node.nodeName.toLowerCase()
       if (name !== 'button') {
@@ -15,28 +16,23 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .g-button-group {
   display: inline-flex;
   vertical-align: middle;
-
   > .g-button {
     border-radius: 0;
-
     &:not(:first-child) {
       margin-left: -1px;
     }
-
     &:first-child {
       border-top-left-radius: var(--border-radius);
       border-bottom-left-radius: var(--border-radius);
     }
-
     &:last-child {
       border-top-right-radius: var(--border-radius);
       border-bottom-right-radius: var(--border-radius);
     }
-
     &:hover {
       position: relative;
       z-index: 1;
